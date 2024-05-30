e725cbf4684666a22e1d05b9402256128eeca1ee
Robin-Mobiux <robin@mobiux.in>
2024-05-31 01:19:57 +0530

    auth-google

--- lib/auth/auth_service.dart
+++ lib/auth/auth_service.dart
@@ -7,6 +7,9 @@ class AuthService {
 
   Future<User?> signInWithGoogle() async {
     try {
+      // Sign out any previously signed-in account
+      await _googleSignIn.signOut();
+
       // Trigger the authentication flow
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
       if (googleUser == null) {
@@ -15,7 +18,8 @@ class AuthService {
       }
 
       // Obtain the auth details from the request
-      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
 
       // Create a new credential
       final credential = GoogleAuthProvider.credential(
@@ -24,7 +28,8 @@ class AuthService {
       );
 
       // Once signed in, return the UserCredential
-      UserCredential userCredential = await _auth.signInWithCredential(credential);
+      UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
       return userCredential.user;
     } catch (e) {
       print(e);
